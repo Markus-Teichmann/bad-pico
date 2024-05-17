d747f1a2af88456105d2f05c0ee042000305732c
Markus Teichmann <markuste@edu.aau.at>
2024-05-17 09:05:41 +0200

    Loesung

--- code.py
+++ code.py
@@ -2,8 +2,36 @@ from payload import run
 import digitalio
 from board import *
 from time import sleep
-
+# Disable autoreload
 import supervisor
+#supervisor.disable_autoreload()
 supervisor.runtime.autoreload = False
 
-run()
+attackMode = False
+attackPin = digitalio.DigitalInOut(GP15) #Der Pin 15 wird zu einem Input oder Output Pin
+attackPin.direction = digitalio.Direction.INPUT #Der Pin wird ein Input Pin
+attackPin.pull = digitalio.Pull.UP #Wenn an diesem Pin kein Input anliegt, können wir hier so Strom anlegen, sodass wir true lesen.
+attackMode = attackPin.value #Der Digitale Wert des Pins wird ausgelesen
+
+if(attackMode == True):
+    print("attackMode == True")
+    red_led = digitalio.DigitalInOut(GP10)
+    red_led.direction = digitalio.Direction.OUTPUT
+    red_led.value = True
+    run()
+    for i in range(0,10):
+        red_led.value = False
+        sleep(0.25)
+        red_led.value = True
+        sleep(0.25)
+    red_led.value = False
+else:
+    print("attackMode == False")
+    green_led = digitalio.DigitalInOut(GP21)
+    green_led.direction = digitalio.Direction.OUTPUT
+    for i in range(0,10):
+        green_led.value = True
+        sleep(0.25)
+        green_led.value = False
+        sleep(0.25)
+    green_led.value = True
--- payload.py
+++ payload.py
@@ -4,4 +4,19 @@ from keyboard_utils.keyboard_character_map import get_code
 from keyboard_utils.keyboard_send_keys import send
 from time import sleep
 
+def repeat():
+    for i in range(0,3):
+        send(get_code(i))
+        send(ENTER)
+
 def run():
+    sleep(5)
+    send(CLOSE, 1)
+    send(RUN, 1)
+    send(get_code('notepad'), 1)
+    send(ENTER, 2)
+    send(get_code('Hallo Welt!'))
+    send(ENTER)
+    repeat()
+    
+#run()
